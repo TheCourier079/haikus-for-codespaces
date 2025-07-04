d27ab6175edd4523abaf80509602048073776e78
TheCourier_079 <rsteward079@gmail.com>
2025-07-04 03:08:29 +0000

    Add markdown package to requirements

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/',(req, res) => {
   res.render('index', {haikus: haikus});
 });
 
